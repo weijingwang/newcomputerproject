0eb1bfe8185415fdee2433fbfa1ece0fb85b4368
Weijing Wang <weijingrocks@gmail.com>
2017-03-12 22:57:25 -0700

    Add files via upload

---
+++ matplotlibtest.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot, dates
+from csv import reader
+from dateutil import parser
+
+with open('table_amzn.csv', 'r') as f:
+    data = list(reader(f))
+
+price = [i[5] for i in data[1::]]
+time = [parser.parse(i[0]) for i in data[1::]]
+
+pyplot.plot(time, price)
+pyplot.show()
